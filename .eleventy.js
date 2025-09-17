bc53ad0e91a81208eda327476e1cad4c90ccabfe
Jolie Menzel <joliemenzel@gmail.com>
2025-09-17 01:36:24 -0400

    fixing css

--- .eleventy.js
+++ .eleventy.js
@@ -35,8 +35,10 @@ module.exports = function(eleventyConfig) {
 
   return {
     dir: { input: "src", includes: "_includes", data: "_data", output: "_site" },
+    pathPrefix: process.env.PATH_PREFIX || "/",
     templateFormats: ["njk","md","html"],
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk"
   };
+
 };
