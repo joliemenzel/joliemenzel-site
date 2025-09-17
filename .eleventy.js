038eb851ac8cc067cb64757652cb4ddbdc07adff
Jolie Menzel <joliemenzel@gmail.com>
2025-09-16 23:14:04 -0400

    super basic clone of my old website

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("filterByTag", function(posts, tag) {
+    return posts.filter(p => (p.data.tags || []).includes(tag));
+  });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+
+  // Collections: posts + tags
+  eleventyConfig.addCollection("posts", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("src/posts/**/*.{md,html}")
+      .sort((a, b) => b.date - a.date);
+  });
+
+  eleventyConfig.addCollection("tagList", (collectionApi) => {
+    const tags = new Set();
+    collectionApi.getFilteredByGlob("src/posts/**/*.{md,html}").forEach(item => {
+      (item.data.tags || []).forEach(t => tags.add(t));
+    });
+    return [...tags].sort();
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    templateFormats: ["njk", "md", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+};
---
+++ src/posts/YYYY-MM-DD-slug.md
@@ -0,0 +1,9 @@
+---
+layout: post.njk
+title: "Test Post"
+date: 2025-09-16
+tags: ["games","professional"]
+hero: "/assets/img/posts/test.jpg"
+permalink: "/post/{{ page.fileSlug }}/"
+---
+Short blurb or caption. Add more HTML/Markdown if you want.
